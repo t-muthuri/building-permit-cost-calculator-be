d7571d77e1305561d1a178ed5864f9c6633a0935
Tressie Muthuri <tressiemuthuri@gmail.com>
2024-05-06 12:01:45 +0300

    refactor: code in calculator views

--- calculator/urls.py
+++ calculator/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('calculator/', views.calculator, name='calculator'),
     path('counties-list/', views.county_list, name='counties'),
 ]
