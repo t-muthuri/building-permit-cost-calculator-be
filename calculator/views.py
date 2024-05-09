bcbd3c3cf8c5d55c28824e13cefac978c323c115
Tressie Muthuri <tressiemuthuri@gmail.com>
2024-05-09 19:07:15 +0300

    chore: use data received from fe to calculate cost

--- calculator/views.py
+++ calculator/views.py
@@ -3,8 +3,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import County, ProjectType
 from .serializers import CountySerializer, ProjectTypeSerializer
+from .management.commands.populate_counties import county_data
 
 # Create your views here.
+
 
 @api_view(['GET'])
 def county_list(request):
@@ -39,18 +41,35 @@ def project_type_list(request):
         status=status.HTTP_200_OK,
     )
 
+
 @api_view(['POST'])
 def calculate_cost(request):
     if request.method == 'POST':
         project_size = request.data.get('size')
-        county_no = request.data.get('county')
-        project_type_no = request.data.get('projectType')
+        county_name = request.data.get('county')
+        project_type_name = request.data.get('projectType')
         construction_cost = request.data.get('cost')
-        
+
         nca_levy = (0.005 * int(construction_cost))
-        
+
+        # total_cost = None
+        for county_no, county_item in county_data.items():
+            # Check if the county name exists in the current county_item
+            if county_item.get(county_name):
+                price = county_item[county_name].get(project_type_name)
+
+                cost_building = (int(project_size)) * price
+                total_cost_approval = nca_levy + cost_building + 10800
+                building_permit = cost_building + 10800
+
+                context = {
+                    'total cost of approval': int(total_cost_approval),
+                    'nca-levy': int(nca_levy),
+                    'building permit': int(building_permit)
+                }
+
         return Response(
-            {"message": "Approval cost calculated successfully!"},
+            {"message": "Approval cost calculated successfully!", "context": context},
             status=status.HTTP_200_OK,
         )
     else:
