983386a92e9b9461aff930440a0494083dd133e1
Tressie Muthuri <tressiemuthuri@gmail.com>
2024-05-15 16:46:24 +0300

    chore: add few endpoints for basic functionality

--- approvals/settings.py
+++ approvals/settings.py
@@ -165,8 +165,6 @@ DJOSER = {
     'SEND_CONFIRMATION_EMAIL': False,
     'SET_USERNAME_RETYPE': True,
     'SET_PASSWORD_RETYPE': True,
-    # 'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
-    # 'USERNAME_RESET_CONFIRM_URL': 'email/reset/confirm/{uid}/{token}',
     'ACTIVATION_URL': 'activate/{uid}/{token}',
     'SEND_ACTIVATION_EMAIL': False,
     'SERIALIZERS': {
