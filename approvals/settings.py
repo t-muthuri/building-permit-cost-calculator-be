e1aef072afb650b4c10a8683ba52b21545eae8bd
Tressie Muthuri <tressiemuthuri@gmail.com>
2024-06-10 11:18:58 +0300

    fix: change from mysqlclient

--- approvals/settings.py
+++ approvals/settings.py
@@ -84,17 +84,23 @@ WSGI_APPLICATION = 'approvals.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'approvals',  # name of db we want to use
+#         'USER': 'root',  # db admin
+#         'PASSWORD': str(os.getenv('DB_PASSWORD')),
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#         }
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'approvals',  # name of db we want to use
-        'USER': 'root',  # db admin
-        'PASSWORD': str(os.getenv('DB_PASSWORD')),
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
