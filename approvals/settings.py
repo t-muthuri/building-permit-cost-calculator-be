b54d4fc3f337c7518cc00de337b2f411e2c9014d
Tressie Muthuri <tressiemuthuri@gmail.com>
2024-06-10 11:33:26 +0300

    chore: change db

--- approvals/settings.py
+++ approvals/settings.py
@@ -84,25 +84,25 @@ WSGI_APPLICATION = 'approvals.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'approvals',  # name of db we want to use
-#         'USER': 'root',  # db admin
-#         'PASSWORD': str(os.getenv('DB_PASSWORD')),
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-#         }
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'approvals',  # name of db we want to use
+        'USER': 'root',  # db admin
+        'PASSWORD': str(os.getenv('DB_PASSWORD')),
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
