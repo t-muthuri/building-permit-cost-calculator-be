d9c0958e35e05726a767b30c2d693d556f2dbfe9
Tressie Muthuri <tressiemuthuri@gmail.com>
2024-06-10 11:48:44 +0300

    deploy: configure db

--- approvals/settings.py
+++ approvals/settings.py
@@ -84,25 +84,39 @@ WSGI_APPLICATION = 'approvals.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'approvals',  # name of db we want to use
-        'USER': 'root',  # db admin
-        'PASSWORD': str(os.getenv('DB_PASSWORD')),
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'approvals',  # name of db we want to use
+#         'USER': 'root',  # db admin
+#         'PASSWORD': str(os.getenv('DB_PASSWORD')),
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#         }
+#     }
+# }
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+# configure to railway
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',  # name of db we want to use
+        'USER': 'root',  # db admin
+        'PASSWORD': str(os.getenv('DB_PASSWORD')),
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '59670',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
