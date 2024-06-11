86343c71436043e1997601faaa7ac47891cbfdf8
Tressie Muthuri <tressiemuthuri@gmail.com>
2024-06-11 11:43:39 +0300

    fix: build error

--- build.sh
+++ build.sh
@@ -1,20 +1,21 @@
-# tell Vercel what commands to run
 #!/bin/bash
 
-# Exit the script on any command with non-zero status
+# Exit immediately if a command exits with a non-zero status
 set -e
 
 # Build the project
 echo "Building the project..."
 
-# pip is available and install dependencies
+# Make pip available and install dependencies
+python3.9 -m ensurepip --upgrade || true
+python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 
-# makemigrtions and migrate
-echo "Make Migration..."
+# Make Migrations and Migrate
+echo "Make Migrations..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
-# collect static files
+# Collect Static Files
 echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
