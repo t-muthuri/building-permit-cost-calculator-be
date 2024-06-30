69dd0678de5918743640fb5c85ec271b837e9b32
t-muthuri <tressiemuthuri@gmail.com>
2024-06-30 06:40:15 +0300

    chore: calculator data

--- approvals/approvals/urls.py
+++ approvals/approvals/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('api/calculator/', include('calculator.urls',)),
+    path('', include('upload.urls')),
 ]
--- approvals/calculator/views.py
+++ approvals/calculator/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import County, ProjectType
+from .serializers import CountySerializer, ProjectTypeSerializer
+from .management.commands.populate_counties import county_data
 
 # Create your views here.
