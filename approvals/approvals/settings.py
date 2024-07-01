54fc2130a74e53cda2a2d4224e4b508bda48cb65
t-muthuri <tressiemuthuri@gmail.com>
2024-07-01 16:30:29 +0300

    chore: secure django project

--- approvals/approvals/settings.py
+++ approvals/approvals/settings.py
@@ -1,15 +1,18 @@
 from pathlib import Path
-import os 
+import os
+from dotenv import load_dotenv 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-31ilv#(swuhc9*o7ztv0+_6^=)&h=2&z@o+d8wyw@a2&_1km9r'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
