0951438c9c9272ff2614b3147868f9b0f3beeae9
t-muthuri <tressiemuthuri@gmail.com>
2024-07-23 08:53:38 +0300

    fix: crash reports

--- approvals/news/views.py
+++ approvals/news/views.py
@@ -19,7 +19,7 @@ class NewsScrapeView(APIView):
     def get(self, request, *args, **kwargs):
         session = requests.Session()
         session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
-        url = "https://www.theonion.com/"
+        url = "https://nation.africa/kenya"
 
         content = session.get(url, verify=False).content
         soup = BSoup(content, "html.parser")
@@ -30,7 +30,12 @@ class NewsScrapeView(APIView):
             link = main['href']
             title = main['title']
             headlines.append({'title': title, 'url': link})
-            # Save the headline to the database
             Headline.objects.update_or_create(title=title, url=link)
         
         return Response(headlines)
+    
+# class HeadlineListView(generics.ListCreateAPIView):
+#     serializer_class = HeadlineSerializer
+
+#     def get_queryset(self):
+#         return Headline.objects.order_by('-id')[:10]
