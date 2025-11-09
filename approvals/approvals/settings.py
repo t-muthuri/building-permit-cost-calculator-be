10a70ebfc85525e536e7f11b6c9abf4161093e1e
t-muthuri <tressiemuthuri@gmail.com>
2025-11-09 07:51:05 +0300

    trial 8

--- approvals/approvals/settings.py
+++ approvals/approvals/settings.py
@@ -17,7 +17,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["approvals-api.up.railway.app"]
 
 
 # Application definition
